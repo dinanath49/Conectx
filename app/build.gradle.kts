33fc557f7b1ac94d5b44e25f7388d33747da3248
dinanath49 <dinanathbarman477@gmail.com>
2026-07-30 20:54:28 +0530

    Remove Google Services plugin alias
    Removed the Google Services plugin alias from the build script.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
-  alias(libs.plugins.google.services)
+ 
 }
 
 android {
@@ -66,7 +66,6 @@ secrets {
   defaultPropertiesFileName = ".env.example"
 }
 
-googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.WARN }
 
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
